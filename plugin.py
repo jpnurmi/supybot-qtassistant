b85f5de0e47caeb38b7f2ac7965e4d58ee5f8337
J-P Nurmi <jpnurmi@gmail.com>
2012-12-20 02:14:42 +0100

    WIP: indexing...

--- plugin.py
+++ plugin.py
@@ -33,13 +33,48 @@ from supybot.commands import *
 import supybot.plugins as plugins
 import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
+import supybot.conf as conf
 
+import glob
+import fuzzydict
+from pysqlite2 import dbapi2 as sqlite3
 
 class QtAssistant(callbacks.Plugin):
     """Add the help for "@plugin help QtAssistant" here
     This should describe *how* to use this plugin."""
-    pass
 
+    def __init__(self, irc):
+        self.__parent = super(QtAssistant, self)
+        self.__parent.__init__(irc)
+        self.dict = fuzzydict.FuzzyDict()
+
+        datadir = conf.supybot.directories.data
+        for qchfile in glob.glob(datadir.dirize("*.qch")):
+            self.dict.update(self._index(qchfile))
+
+    def _index(self, file):
+        dict = fuzzydict.FuzzyDict()
+        db = sqlite3.connect(file)
+        cursor = db.cursor()
+        # IndexTable: Id, Name, Identifier, NamespaceId, FileId, Anchor
+        # FileNameTable: FolderId, Name, FileId, Title
+        # FolderTable: Id, Name, NamespaceId
+        cursor.execute('SELECT * FROM IndexTable, FileNameTable, FolderTable '
+                       'WHERE IndexTable.FileId = FileNameTable.FileId')
+        rows = cursor.fetchall()
+        for row in rows:
+            # (200, u'XQuery', u'XQuery', 1, 61, None, 1, u'xmlprocessing.html', 61, u'qtxmlpatterns : XQuery')
+            print(row)
+#            entry = {}
+#            entry['name'] = row[1]
+#            entry['id'] = row[2]
+#            entry['anchor'] = row[5]
+#            entry['file'] = row[7]
+#            entry['title'] = row[9]
+#            dict[entry['name']] = entry
+#            dict[entry['id']] = entry
+#            dict[entry['file']] = entry
+        return dict
 
 Class = QtAssistant
 
